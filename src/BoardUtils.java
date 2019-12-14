274a2755d509f716686ea473941be14c1648ef9e
Eoin <daly.e@husky.neu.edu>
2019-12-14 14:05:42 -0500

    utils update
    new reset board method and linking now links cells for an infinite world

--- src/BoardUtils.java
+++ src/BoardUtils.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
 import tester.*;
@@ -8,7 +9,11 @@ import javalib.worldimages.*;
 
 public class BoardUtils {
 	// int for limit of the for loops
-	int limit = LifeWorld.SIZE;
+	int limit;
+	
+	BoardUtils(LifeWorld world) {
+		this.limit = world.SIZE;
+	}
 	
 	// Create 2D matrix of cells
 	public ArrayList<ArrayList<Cell>> makeCells() {
@@ -24,51 +29,90 @@ public class BoardUtils {
 	}
 	
 	// Links cells to their neighbors
-	public void linkCells(ArrayList<ArrayList<Cell>> cells) {
+	public void linkNeighbors(ArrayList<ArrayList<Cell>> cells) {
+		int max = this.limit - 1;
 		for (int r = 0; r < this.limit; r ++) {
 			for (int c = 0; c < this.limit; c ++) {
 				Cell current = cells.get(r).get(c);
 				if (c != 0) {
 					current.left = cells.get(r).get(c - 1);
-					current.neighbors.add(current.left);
 					
 					if (r != 0) {
 						current.topLeft = cells.get(r - 1).get(c - 1);
-						current.neighbors.add(current.topLeft);
 					}
 					if (r + 1 != this.limit) {
 						current.botLeft = cells.get(r + 1).get(c - 1);
-						current.neighbors.add(current.botLeft);
 					}
 				}
 				
-				if (c + 1 != this.limit) {
+				if (c != max) {
 					current.right = cells.get(r).get(c + 1);
-					current.neighbors.add(current.right);
 					
 					if (r != 0) {
 						current.topRight = cells.get(r - 1).get(c + 1);
-						current.neighbors.add(current.topRight);
 					}
 					if (r + 1 != this.limit) {
 						current.botRight = cells.get(r + 1).get(c + 1);
-						current.neighbors.add(current.botRight);
 					}
 				}
 				
 				if (r != 0) {
 					current.top = cells.get(r - 1).get(c);
-					current.neighbors.add(current.top);
+				}
+				else {
+					current.top = cells.get(max).get(c);
 				}
 				
 				if (r + 1 != this.limit) {
 					current.bot = cells.get(r + 1).get(c);
-					current.neighbors.add(current.bot);
 				}
+				else {
+					current.bot = cells.get(0).get(c);
+				}
+				
+				int mirrorLeft = ((c - 1) < 0 ? max : c - 1);
+				int mirrorRight = ((c + 1) == this.limit ? 0 : c + 1);
+				int mirrorTop = ((r - 1) < 0 ? max : r - 1);
+				int mirrorBot = ((r + 1) == this.limit ? 0 : r + 1);
+
+				if (current.topLeft == null) {
+					current.topLeft = cells.get(mirrorTop).get(mirrorLeft);
+				}
+				if (current.top == null) {
+					current.top = cells.get(max).get(c);
+				}
+				if (current.topRight == null) {
+					current.topRight = cells.get(mirrorTop).get(mirrorRight);
+				}
+				if (current.left == null) {
+					current.left = cells.get(r).get(max);
+				}
+				if (current.right == null) {
+					current.right = cells.get(r).get(0);
+				}
+				if (current.botLeft == null) {
+					current.botLeft = cells.get(mirrorBot).get(mirrorLeft);
+				}
+				if (current.bot == null) {
+					current.bot = cells.get(0).get(c);
+				}
+				if (current.botRight == null) {
+					current.botRight = cells.get(mirrorBot).get(mirrorRight);
+				}
+				
+				ArrayList<Cell> neighborList = current.neighbors;
+				neighborList.add(current.topLeft);
+				neighborList.add(current.top);
+				neighborList.add(current.topRight);
+				neighborList.add(current.left);
+				neighborList.add(current.right);
+				neighborList.add(current.botLeft);
+				neighborList.add(current.bot);
+				neighborList.add(current.botRight);
 			}
 		}
 	}
-	
+
 	// Draws cells onto given scene
 	public void drawCells(ArrayList<ArrayList<Cell>> cells, WorldScene background) {
 		for (int r = 0; r < this.limit; r ++) {
@@ -82,6 +126,17 @@ public class BoardUtils {
 		}
 	}
 	
+	// resets the cells
+	public void reset(ArrayList<ArrayList<Cell>> cells) {
+		for (ArrayList<Cell> row : cells) {
+			for (Cell c : row) {
+				c.alive = false;
+				c.nextLife = false;
+			}
+		}
+	}
+	
+	// Update whether or not the cell's should be alive or dead in the next tick
 	public void processLife(ArrayList<ArrayList<Cell>> cells) {
 		for (ArrayList<Cell> row : cells) {
 			for (Cell c : row) {
